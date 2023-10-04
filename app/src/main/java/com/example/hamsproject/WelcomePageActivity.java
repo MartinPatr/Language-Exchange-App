813041039fa70df0e3c51a50c9adc74c5d586248
Henry Li <henryfli2004@gmail.com>
2023-10-04 15:19:35 -0400

    updated welcome page

--- app/src/main/java/com/example/hamsproject/WelcomePageActivity.java
+++ app/src/main/java/com/example/hamsproject/WelcomePageActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class WelcomePage extends AppCompatActivity {
+public class WelcomePageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
