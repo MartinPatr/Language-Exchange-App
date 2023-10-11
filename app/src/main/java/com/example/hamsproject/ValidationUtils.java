4e0573f8647a1da449b55dbf45de5b26cf0e8a1b
MartinPatr <martinpatrouchev@gmail.com>
2023-10-10 23:48:12 -0400

    Update ValidationUtils.java to state which fields are empty in alert

--- app/src/main/java/com/example/hamsproject/ValidationUtils.java
+++ app/src/main/java/com/example/hamsproject/ValidationUtils.java
@@ -3,29 +3,44 @@ package com.example.hamsproject;
 import android.content.Context;
 import android.widget.Toast;
 import java.util.Map;
+import java.util.ArrayList;
 
 public class ValidationUtils {
 
     /* 
      * This method checks if all the fields are filled in and valid
      * @param context: Context of the activity
-     * @param patientInfo: Map<String, String> that contains all the patient information
+     * @param info: Map<String, String> that contains all the patient information
      * @return boolean: true if all the fields are filled in, false otherwise
      */
     public static boolean isValidated(Context context, Map<String, String> info) {
+        ArrayList<String> requiredFields = new ArrayList<>();
+        StringBuilder emptyFieldsMessage = new StringBuilder();
+
         // Iterate through the Map
         for (String key : info.keySet()) {
-            // This checks if the field is empty
+            // Adds the key to the requiredFields ArrayList if the value is empty
             if (info.get(key).isEmpty()) {
-                Toast.makeText(context, "Please fill the following fields", Toast.LENGTH_SHORT).show();
-                return false;
-            }
-            // This checks if the email is valid
-            else if (key.equals("Email") && !info.get(key).contains("@") && !info.get(key).contains(".")) {
-                Toast.makeText(context, "Please enter a valid email", Toast.LENGTH_SHORT).show();
-                return false;
+                requiredFields.add(key);
+                emptyFieldsMessage.append(key).append(", ");
             }
         }
+        // This checks if there are any empty fields
+        if (!requiredFields.isEmpty()) {
+            // Remove the trailing comma and space
+            emptyFieldsMessage.deleteCharAt(emptyFieldsMessage.length() - 2);
+            String message = "Please fill in the following fields: " + emptyFieldsMessage.toString();
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        // This checks if the email is valid
+        if (!info.get("Email").contains("@") || !info.get("Email").contains(".")) {
+            Toast.makeText(context, "Please enter a valid email", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        
         return true;
     }
 }
+
