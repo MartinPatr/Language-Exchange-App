e5bf22e2ff853b22de00709e0222b98b860e21ae
Kylan Thurairajah <kylan.r.thurairajah@gmail.com>
2023-10-11 09:21:02 -0400

    account file added

---
+++ app/src/main/java/com/example/hamsproject/Account.java
@@ -0,0 +1,51 @@
+package com.example.hamsproject;
+
+public class Account {
+
+
+    public String firstName;
+    public String lastName;
+    public String username;
+    public int password;
+    public int phone;
+    public int address;
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName() {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName() {
+        this.lastName = lastName;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername() {
+        this.username = username;
+    }
+    public int getPassword() {
+        return password;
+    }
+    public void setPassword() {
+        this.password = password;
+    }
+    public int getPhone() {
+        return phone;
+    }
+    public void setPhone() {
+        this.phone = phone;
+    }
+    public int getAddress() {
+        return address;
+    }
+    public void setAddress() {
+        this.address = address;
+    }
+
+
+}
