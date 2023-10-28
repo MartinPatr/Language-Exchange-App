958e9b22e304e2a560cae3ca82d302e49ef2d37d
Henry Li <henryfli2004@gmail.com>
2023-10-28 09:38:41 -0400

    code

--- app/src/main/java/com/example/hamsproject/AdminPendingInfoActivity.java
+++ app/src/main/java/com/example/hamsproject/AdminPendingInfoActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -17,7 +18,10 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.net.URI;
 import java.util.ArrayList;
+
+
 
 public class AdminPendingInfoActivity extends AppCompatActivity {
 
@@ -41,6 +45,9 @@ public class AdminPendingInfoActivity extends AppCompatActivity {
             public void onClick(View v) {
                 changeStatus(accountID, "Approved");
                 Intent intent = new Intent(AdminPendingInfoActivity.this, AdminPendingActivity.class);
+
+
+
                 startActivity(intent);
             }
         });
@@ -49,6 +56,8 @@ public class AdminPendingInfoActivity extends AppCompatActivity {
             public void onClick(View v) {
                 changeStatus(accountID, "Denied");
                 Intent intent = new Intent(AdminPendingInfoActivity.this, AdminPendingActivity.class);
+
+
                 startActivity(intent);
             }
         });
@@ -173,4 +182,7 @@ public class AdminPendingInfoActivity extends AppCompatActivity {
             });
         }
     }
+
+
+
 }
